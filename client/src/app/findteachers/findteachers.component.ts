31c83c3a090293147b8697be1514a092906c5a50
Ernesmh <ernesmh@gmail.com>
2017-12-08 23:45:10 +0100

    front

--- client/src/app/findteachers/findteachers.component.ts
+++ client/src/app/findteachers/findteachers.component.ts
@@ -11,7 +11,6 @@ import { AgmCoreModule } from '@agm/core'
 })
 export class FindteachersComponent implements OnInit {
   teacher: Array<Object>=[];
-  title: string = 'My first AGM project';
   lat: number = 40.392597;
   lng: number = -3.698288;
   constructor(public router:Router, public authService: AuthService, public userService: UserService, public route:ActivatedRoute) { }
