ad7f800ded3a5a116d65e64e7b15f34015231b5f
Ernesmh <ernesmh@gmail.com>
2017-12-14 14:14:47 +0100

    otra vez

--- passport/index.js
+++
@@ -1,8 +0,0 @@
-const passport = require("passport");
-require('./localStrategy');
-require('./serializers');
-
-module.exports = (app) => {
-  app.use(passport.initialize());
-  app.use(passport.session());
-};
